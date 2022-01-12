4b0df936fbb9f3d2195d0d33da048c8ac8c84e86
Petrarkius <greeinq@gmail.com>
2022-01-12 09:19:50 +0200

    test requseting data to app server (failed)

---
+++ client/assets/js/test.js
@@ -0,0 +1,24 @@
+const oururl = 'https://localhost:5001'
+
+const resource = '/Players'
+
+async function getPlayers () {
+  await fetch(oururl + resource, {
+    // mode: 'no-cors'
+  })
+    .then(res => console.log(res))
+}
+
+async function getPlayersXHR () {
+  const req = new XMLHttpRequest()
+  req.open('GET', oururl + resource)
+  req.setRequestHeader('Content-Type', 'application/json')
+  req.setRequestHeader('mode')
+  req.send()
+
+  req.addEventListener('load', () => {
+    console.log(req.response)
+  })
+}
+
+getPlayers()
